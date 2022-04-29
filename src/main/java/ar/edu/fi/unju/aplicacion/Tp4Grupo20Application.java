bcf8a7c4ca4795e84bdfc974752a27f75933fed2
maxi1 <maxi1@DESKTOP-G09S1M5>
2022-04-29 15:19:02 -0300

    Subida del TP, lo hicimos en clase.

---
+++ src/main/java/ar/edu/fi/unju/aplicacion/Tp4Grupo20Application.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo20Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/fi/unju/aplicacion/Tp4Grupo20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
