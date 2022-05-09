ac96ed2c0ba013f97800df85056123268242c943
Fran-2811fer <pacnhofer1122@gmail.com>
2022-05-09 20:54:28 -0300

    Clase y atributos realizados

--- src/main/java/ar/edu/unju/fi/model/Curso.java
+++ src/main/java/ar/edu/unju/fi/model/Curso.java
@@ -5,5 +5,9 @@ public class Curso {
 	private String Titulo;
 	private String Categoria;
 	private int Fecha_inicio;
+	private int Fecha_fin;
+	private int Cantidad_horas;
+	private String Modalidad;
+	private String Docente;
 
 }
