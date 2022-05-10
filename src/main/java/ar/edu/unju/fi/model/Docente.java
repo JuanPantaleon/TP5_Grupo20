d5b9678e9d121baab91680f6a99eaa2971cd4e11
Fran-2811fer <pacnhofer1122@gmail.com>
2022-05-10 15:55:27 -0300

    Creación de la clase docente y sus atributos

---
+++ src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.model;
+
+public class Docente {
+
+	public static void main(String[] args) {
+		private String Nombre;
+		private String Apellido;
+		private int Legajo;
+		private int Telefono;
+		private int Email;
+
+	}
+
+}
