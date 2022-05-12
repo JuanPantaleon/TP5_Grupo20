00ac0b1095fabcb92340e358484b74ca4bacb0f9
Fran-2811fer <pacnhofer1122@gmail.com>
2022-05-12 16:02:22 -0300

    creacion de la clase beca

--- src/main/java/ar/edu/unju/fi/model/Beca.java
+++ src/main/java/ar/edu/unju/fi/model/Beca.java
@@ -3,7 +3,11 @@ package ar.edu.unju.fi.model;
 public class Beca {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private int Codigo;
+		private int Curso;
+		private int Fechainicio;
+		private int Fechacierre;
+		private String Estado; 
 
 	}
 
