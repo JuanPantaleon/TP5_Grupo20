d2df247d2fb229d4f6f606ae38350377d2533156
Fran-2811fer <pacnhofer1122@gmail.com>
2022-05-12 15:53:03 -0300

    creando la clase alumno

---
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.model;
+
+public class Alumno {
+
+	public static void main(String[] args) {
+		private String Nombre;
+		private String Apellido;
+		private String Email;
+		private int DNI;
+		private int Telefono;
+		
+
+	}
+
+}
